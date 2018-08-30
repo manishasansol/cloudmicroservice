ef08755b91a9b0ff95bfcda268ea56b3a21b2bc0
manishasansol <manish.asansol.20@gmail.com>
2018-08-30 01:50:40 +0100

    created initial set up

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-west-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster manish-asansol-microservice-demo
+ecs-cli configure profile --profile-name default --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli configure --region us-west-2 --cluster manish-asansol-microservice-demo
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
